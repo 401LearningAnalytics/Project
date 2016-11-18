a1a0430ef84e40129ee19745dbe82fbdb3402e9b
root <root@401-learning-analytics.cloud.cybera.ca>
2016-11-18 07:04:47 +0000

    quiz summary per course almost there

---
+++ server_side/upload.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import sys
+import os
+import subprocess
+import time
+
+filename = sys.argv[1]
+p = subprocess.Popen(["unzip", filename, "-dintro"], stdout=subprocess.PIPE)
+p.communicate()
+
+subprocess.call("rm intro/*.html intro/*.pdf", shell=True)
+p = subprocess.Popen(["ls", "intro"],stdout=subprocess.PIPE, universal_newlines=True)
+output, error = p.communicate()
+file_list = str(output).split("\n")
+while len(file_list) != 0:
+    for i in file_list:
+        k = "intro/"+i
+        p = subprocess.Popen(["php","-f","uploadtodb.php",k],stdout=subprocess.PIPE, universal_newlines=True)
+        out, err = p.communicate()
+        if ("Error" in out) or ("error" in out):
+                print(k)
+        else:
+                file_list.remove(i)
+
---
+++ server_side/uploadtodb.php
@@ -0,0 +1,50 @@
+<?php
+   $filename = $argv[1];
+                 //Print file details
+             $file = fopen($filename, 'r');
+             $i = 0;
+             while (($line = fgetcsv($file)) !== FALSE) {
+                              //$line is an array of the csv elements
+             $n = count($line);
+             $j = 0;
+             $string = "INSERT into ".$basename." VALUES(";
+            if ($i > 0){
+                  while($j < $n){
+                    if (strcmp($line[$j], "") != 0 & strcmp($line[$j], "t") != 0 && strcmp($line[$j], "f") != 0){
+                      $string = $string."\"".$line[$j]."\"";
+                    }else if(strcmp($line[$j], "f") == 0){
+                      $string = $string."false";
+                    }else if(strcmp($line[$j], "t") == 0){
+                      $string = $string."true";
+                    }
+                    if (strcmp($line[$j], "") == 0){$string = $string."NULL";}
+                    if ($j + 1 < $n){$string = $string.",";}
+                    $j++;
+                  }
+                $string = $string.");";
+                echo $string."<br>";
+                $servername = "localhost";
+                $username = "Wei";
+                $password = "123456";
+                // Create connection
+                $conn = new mysqli($servername, $username, $password);
+                // Check connection
+                if ($conn->connect_error) {
+                   die("Connection failed: " . $conn->connect_error."<br>");
+                }
+                $sql = "use learner;";
+                if (!($conn->query($sql) === TRUE)) {
+                   echo "Error switching database: " . $conn->error."<br>";
+                }
+                if ($conn->query($string) === TRUE) {
+                   echo "Data inserted successfully"."<br>";
+                } else {
+                   echo "Error inserting data: " . $conn->error."<br>";
+                }
+                $conn->close();
+           }
+             echo "<br>";
+             i++ ;
+}
+fclose($file);
+?>
