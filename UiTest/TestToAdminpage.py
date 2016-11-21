43628eea7f90ff616041b5af23d43f1d1996d618
Main User Account <omokdad@yahoo.com>
2016-11-20 23:13:19 -0700

    still working on tests

--- UiTest/TestToAdminpage.py
+++ UiTest/TestToAdminpage.py
@@ -5,13 +5,14 @@ from selenium.webdriver.common.keys import Keys
 
 class TestToAdminPage(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.FireFox()
+        self.driver = webdriver.Firefox()
     
     def test_to_admin_page(self):
         driver = self.driver
         driver.get("http://162.246.157.230")
-        element = driver.find_elementbyxpath("//input[@value='Admin']")
-        self.assertIn("CSV Uploading Page", driver.title)
+        element = driver.find_element_by_xpath("//input[@value='   Admin   ']")
+        element.click()
+        self.assertTrue
     
     def tearDown(self):
         self.driver.close()
--- UiTest/TestToCourse1Page.py
+++ UiTest/TestToCourse1Page.py
@@ -5,15 +5,16 @@ from selenium.webdriver.common.keys import Keys
 
 class TestToCourse1Page(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.FireFox()
+        self.driver = webdriver.Firefox()
     
     def test_to_course1_page(self):
         driver = self.driver
         driver.get("http://162.246.157.230")
-        element = driver.find_elementbyxpath("//input[@value='Instructor']")
+        element = driver.find_element_by_xpath("//input[@value=' Instructor']")
         element.click()
-        element = driver.find_elementbyxpath("//div[@id='users']/ul/table/tbody/tr/td/button")
-        self.assertIn("Introduction to Software Product Management", driver.title)
+        element = driver.find_element_by_xpath("//button[contains(.,Introduction to Software Product Management)]")
+        element.click()
+        self.assertTrue()
     
     def tearDown(self):
         self.driver.close()
